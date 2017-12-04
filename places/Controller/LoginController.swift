1614c95d6a1a1dfd713d92fba5f127f27ba565ad
Jonathan Hernandez <jonathan@Jonathans-MacBook-Air.local>
2017-12-04 12:52:05 -0700

    create Login interface

---
+++ places/Controller/LoginController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  places
+//
+//  Created by Jonathan Hernandez on 12/3/17.
+//  Copyright © 2017 Jonathan Hdez. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
