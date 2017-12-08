9083ce692d75b178955765dce69c1f896cb9b54f
Jonathan Hernandez <jonathan@Jonathans-MacBook-Air.local>
2017-12-08 00:27:36 -0700

    move view back down to its original place when the keyboard disappears

--- places/Controller/LoginController.swift
+++ places/Controller/LoginController.swift
@@ -21,7 +21,22 @@ class LoginController: UIViewController, SignUpDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+        
         registerNotificationObservers()
+    }
+    
+    /**
+         Calls this function when the tap is recognized.
+     */
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     func registerNotificationObservers() {
